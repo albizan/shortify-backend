cff8ab463618d60a65a4711dff43c997cc028e3a
Alberto Zanotti <albizan@hotmail.it>
2020-01-22 20:15:14 +0100

    Enable cors and preflight

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
-import config from 'config';
 import { Logger } from '@nestjs/common';
 
 const logger = new Logger('Bootstrap');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  });
   logger.log('CORS enabled');
   await app.listen(process.env.PORT);
   logger.log(`App listening on port ${process.env.PORT}`);
