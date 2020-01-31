f344f5a983b9399ef6160d39651af8542e332ae6
Alberto <albizan@hotmail.it>
2020-01-31 16:38:33 +0100

    Use prettier

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-
 // Avoid dotenv.config in production environment
 import * as dotenv from 'dotenv';
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
