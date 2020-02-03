48f0731d853684b60b39ff362d6015849fcbbbb1
unknown <albizan@hotmail.it>
2020-02-03 18:08:15 +0100

    Test sanitizeUser

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -6,6 +6,8 @@ import { LinkService } from '../link/link.service';
 
 describe('UserService', () => {
   let service: UserService;
+
+  // Define Mocks
   const mockUserRepository = new (jest.fn(() => ({
     create: jest.fn(),
     save: jest.fn()
@@ -15,6 +17,13 @@ describe('UserService', () => {
     name: 'Test Name',
     email: 'Test Email',
     password: 'Test Password'
+  }
+  const mockUser = {
+    id: 'Test Id',
+    name: 'Test Name',
+    email: 'Test Email',
+    password: 'Test Password',
+    links: []
   }
 
   beforeEach(async () => {
@@ -26,18 +35,16 @@ describe('UserService', () => {
   });
   afterEach(() => {
     jest.clearAllMocks();
-  });
+  })
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe('Register Method', () => {
+  // Test Register Method
+  describe('register Method', () => {
     it('Should create new users if no errors are thrown', async () => {
-      mockUserRepository.create.mockReturnValue({
-        id: 'Test Id',
-        links: []
-      })
+      mockUserRepository.create.mockReturnValue(mockUser)
       mockUserRepository.save.mockResolvedValue('User saved')
 
       expect(mockUserRepository.create).not.toHaveBeenCalled()
@@ -48,6 +55,7 @@ describe('UserService', () => {
       expect(user.email).toBe('Test Email')
       expect(mockUserRepository.create).toHaveBeenCalledTimes(1)
       expect(mockUserRepository.save).toHaveBeenCalledTimes(1)
+      expect(mockUserRepository.save).toHaveBeenCalledWith(mockUser)
     })
 
     it('Should throw Internal Server Error if userRepository.save throws error', async () => {
@@ -60,27 +68,48 @@ describe('UserService', () => {
       expect(mockUserRepository.create).not.toHaveBeenCalled()
       expect(mockUserRepository.save).not.toHaveBeenCalled()
       // service.register is an async function, I need to use rejects before toThrow
-      await expect(service.register(mockRegisterDto)).rejects.toThrowError('Internal Error, user was not saved')
+      expect(service.register(mockRegisterDto)).rejects.toThrowError('Internal Error, user was not saved')
       expect(mockUserRepository.create).toHaveBeenCalledTimes(1)
       expect(mockUserRepository.save).toHaveBeenCalledTimes(1)
     })
 
     it('Should throw Conflict Exception if userRepository.save throws error with code 23505', async () => {
-      const error: any = new Error()
-      error.code = '23505'
       mockUserRepository.create.mockReturnValue({
         id: 'Test Id',
         links: []
       })
-      mockUserRepository.save.mockRejectedValue(error);
+      mockUserRepository.save.mockRejectedValue({
+        code: '23505'
+      });
       
 
       expect(mockUserRepository.create).not.toHaveBeenCalled()
       expect(mockUserRepository.save).not.toHaveBeenCalled()
       // service.register is an async function, I need to use rejects before toThrow
-      await expect(service.register(mockRegisterDto)).rejects.toThrowError('Username already exists')
+      expect(service.register(mockRegisterDto)).rejects.toThrowError('Username already exists')
       expect(mockUserRepository.create).toHaveBeenCalledTimes(1)
       expect(mockUserRepository.save).toHaveBeenCalledTimes(1)
     })
+  })
+
+  // Test SanitizeUser Method
+  describe('sanitizeUser Method', () => {
+    it('Should sanitize users', () => {
+      let user: any = new User()
+      const sanitized = {
+        id: "Test Id",
+        name: "Test Name",
+        email: "Test Email",
+      }
+      user = {
+        ...sanitized,
+        password: 'Test Password',
+        otherThingsToBeSanitized: 'Test'
+      }
+      const sanitizedUser = service.sanitizeUser(user)
+      expect(sanitizedUser).toEqual(sanitized)
+    })
+    
+
   })
 });
